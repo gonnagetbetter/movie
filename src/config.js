854f31475295ebac575669e40b52f0c64541ebb7
gonnagetbetter <denissss906@gmail.com>
2025-07-13 18:28:28 +0300

    added config file

---
+++ src/config.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  PORT: process.env.PORT || 8000,
+
+  JWT_SECRET: process.env.JWT_SECRET || 'default_secret_key',
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
+
+  DB_DIALECT: process.env.DB_DIALECT || 'sqlite',
+  DB_HOST: process.env.DB_HOST || './dev.sqlite',
+};
+
+export const { PORT, JWT_SECRET, JWT_EXPIRES_IN, DB_DIALECT, DB_HOST } = config;
